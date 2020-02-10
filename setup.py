c7c92421962a4bfb371d34a991be8d05ec593be6
malyabee <malyadri.beegala@gmail.com>
2020-02-10 19:32:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ https://github.com/pypa/sampleproject
 """
 
 
-""""
+"""
 io.open is needed for projects that support Python 2.7
 It ensures open() defaults to text mode with universal newlines,
 and accepts an argument to specify the text encoding
 Python 3 only projects can skip this import
-""""
-from io import open
+"""
+#from io import open
 
 from os import path
 # Always prefer setuptools over distutils
